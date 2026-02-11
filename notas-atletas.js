a535471280d08f2878c2cc737d7c9895b6495de7
jvcavalho <jvcnatal.carvalho@gmail.com>
2026-02-11 07:43:41 -0300

    projeto notas
    concluido

--- notas-atletas.js
+++ notas-atletas.js
@@ -16,3 +16,35 @@ let atletas = [
    notas: [10, 10, 10, 9, 9.5]
  }
 ];
+
+function calcularMediaAtletas(objAtletas) {
+
+    for (let i = 0; i < objAtletas.length; i++) {
+
+        let nome = objAtletas[i].nome;
+        let notas = objAtletas[i].notas;
+
+        // Ordena as notas do menor para o maior
+        notas.sort((a, b) => a - b);
+
+        // Remove a menor e a maior nota
+        let notasValidas = notas.slice(1, 4);
+
+        // Soma das notas válidas
+        let soma = 0;
+        notasValidas.forEach(function(nota) {
+            soma = soma + nota;
+        });
+
+        // Calcula a média
+        let media = soma / notasValidas.length;
+
+        // Exibe o resultado
+        console.log(`Atleta: ${nome}`);
+        console.log(`Notas Obtidas: ${notas}`);
+        console.log(`Média Válida: ${media}`);
+        console.log("");
+    }
+}
+
+calcularMediaAtletas(atletas);
