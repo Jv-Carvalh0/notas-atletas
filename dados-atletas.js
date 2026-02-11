932371b9a2137a39fd1a6ff721e4162fed903d25
jvcavalho <jvcnatal.carvalho@gmail.com>
2026-02-11 08:15:03 -0300

    inicio
    inicio

---
+++ dados-atletas.js
@@ -0,0 +1,18 @@
+// Declara o atleta
+class Atleta{
+  constructor(nome, idade, peso, altura, notas){
+    this.nome = nome
+    this.idade = idade
+    this.peso = peso
+    this.altura = altura
+    this.notas = notas
+  }
+
+}
+
+
+const atleta = new Atleta("Cesar Abascal",
+    30, 80, 1.70,
+    [10, 9.34, 8.42, 10, 7.88]);
+
+    console.log(atleta)
